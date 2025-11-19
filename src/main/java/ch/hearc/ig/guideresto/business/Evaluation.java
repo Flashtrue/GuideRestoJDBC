70c359e00539ca6c33e6fd884a53ca6e40a30c2a
Jordan <jordanogay@gmail.com>
2025-11-19 09:09:42 +0100

    Add JPA annotations for entity mapping in Evaluation class

--- src/main/java/ch/hearc/ig/guideresto/business/Evaluation.java
+++ src/main/java/ch/hearc/ig/guideresto/business/Evaluation.java
@@ -2,9 +2,17 @@ package ch.hearc.ig.guideresto.business;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
 /**
  * @author cedric.baudet
  */
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Evaluation implements IBusinessObject {
 
     private Integer id;
